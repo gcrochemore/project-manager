b8e29325adaa0219362ea1a0c744f2622648cb6b
Guillaume CROCHEMORE <guillaume.crochemore@gmail.com>
2018-01-23 23:52:18 +0100

    Ajout confirmable & lockable sur devise

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20180123215055) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false
